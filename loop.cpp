8b2304f4bd9e4587e52a5f621fcaeb0149125853
gilsonlevi <gilsonlevi328@gmail.com>
2025-03-20 10:06:44 -0300

    Usando referencias

---
+++ loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+        int a = 5;
+        int x = 2;
+
+while(2>1){
+
+        cout << pow(a,x);
+
+        x++;
+}
+
+}
