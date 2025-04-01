113ce4e202b85935dcae9efa5584d604a0e07f62
gilsonlevi <gilsonlevi328@gmail.com>
2025-04-01 12:16:00 -0300

    Revisao

---
+++ revisao/calculadora.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string opcao;
+    double num1;
+    double num2;
+
+    while(true){
+        cout << "Digite uma operacao(+, -, /, *): ";
+        cin >> opcao;
+
+        if(opcao != "+" && opcao != "-" && opcao != "/" && opcao != "*" ){
+            break;
+        }
+
+        cout << "Digite o primeiro numero: ";
+        cin >> num1;
+
+        cout << "Digite o segundo numero: ";
+        cin >> num2;
+
+        if(opcao == "+"){
+            cout << "Valor da operacao: "<< num1 << " + " << num1 << " = "<< num1 + num2 << "\n";
+        }else if (opcao == "-"){
+            cout << "Valor da operacao: "<< num1 << " - " << num1 << " = "<< num1 - num2 << "\n";
+        }else if (opcao == "/"){
+            cout << "Valor da operacao: "<< num1 << " / " << num1 << " = "<< num1 / num2 << "\n";
+        }else {
+            cout << "Valor da operacao: "<< num1 << " * " << num1 << " = "<< num1 * num2 << "\n";
+        }
+    }
+
+}
