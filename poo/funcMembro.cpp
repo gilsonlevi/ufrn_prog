bc5b79f6139d47da740acb81c1c550858764e3d0
GILSON LEVI DE LIMA ALVES <gilson.alves.701@ad.ufrn.br>
2025-04-08 10:02:28 -0300

    Começando poo em c++

---
+++ poo/funcMembro.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+    class myClass {
+    public:
+        int i;
+
+        void f1();
+    };
+
+    void myClass::f1(){
+
+        i = 7;
+    }
+
+
+int main()
+{
+
+    return 0;
+}
