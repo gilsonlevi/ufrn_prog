1b4d9a7a1f42580b0ec0836625df802503b6f43d
GILSON LEVI DE LIMA ALVES <gilson.alves.701@ad.ufrn.br>
2025-04-10 09:36:43 -0300

    Aprendendo construtores

---
+++ poo/construtores.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Pessoa {
+private:
+    string nomePessoa;
+    int idadePessoa;
+
+public:
+
+    Pessoa(string nome, int idade);
+
+    string getNome(){
+
+        return nomePessoa;
+    }
+
+    void setNome(string nome){
+        nomePessoa = nome;
+    }
+
+
+};
+
+// Constrtuor membro de classe Pessoa
+Pessoa::Pessoa(string nome, int idade): nomePessoa(nome), idadePessoa(idade)
+{
+}
+
+
+
+
+int main(){
+
+
+    Pessoa p1("Levi", 20);
+    cout << p1.getNome();
+
+
+}
